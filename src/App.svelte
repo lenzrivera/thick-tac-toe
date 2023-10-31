57ff521d6d54a7fbf3085abfc8ba30d9001f3281
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2023-10-31 23:25:43 +0800

    Add misc. quality-of-life styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,54 @@
-<div class="board">
-  <div class="pannable_board">
-    <div class="tile x"></div>
-    <div class="tile o"></div>
-    <div class="tile covered"></div>
+<div class="app">
+  <div class="board">
+    <div class="pannable_board">
+      <div class="tile x"></div>
+      <div class="tile o"></div>
+      <div class="tile covered"></div>
 
-    {#each [...Array(31 * 31 - 3)] as _}
-      <div class="tile"></div>
-    {/each}
+      {#each [...Array(31 * 31 - 3)] as _}
+        <div class="tile"></div>
+      {/each}
+    </div>
   </div>
+
+  <div class="main_modal">
+    <h1>thick-tac-toe</h1>
+
+    <div class="join_info">
+      <p>join link:</p>
+      <input value={location.href} readonly />
+      <button
+        ><span class="material-symbols-outlined">content_copy</span></button
+      >
+    </div>
+
+    <div class="description">
+      <p>
+        lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+        nesciunt mollitia fugit explicabo, aperiam quidem officia, reiciendis
+        distinctio doloribus vitae sint perspiciatis? Vitae exercitationem
+        quaerat eligendi dolor eius cupiditate sed.
+      </p>
+      <p>
+        lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum,
+        maiores omnis. Itaque consectetur quasi dolorem qui assumenda inventore
+        nihil aspernatur beatae repellat in debitis, porro modi laudantium eos,
+        exercitationem laborum?
+      </p>
+    </div>
+  </div>
+
+  <div class="fog hidden"></div>
 </div>
-
-<div class="main_modal">
-  <h1>thick-tac-toe</h1>
-
-  <div class="join_info">
-    <p>join link:</p>
-    <input value={location.href} readonly />
-    <button><span class="material-symbols-outlined">content_copy</span></button>
-  </div>
-
-  <div class="description">
-    <p>
-      lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nesciunt
-      mollitia fugit explicabo, aperiam quidem officia, reiciendis distinctio
-      doloribus vitae sint perspiciatis? Vitae exercitationem quaerat eligendi
-      dolor eius cupiditate sed.
-    </p>
-    <p>
-      lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum, maiores
-      omnis. Itaque consectetur quasi dolorem qui assumenda inventore nihil
-      aspernatur beatae repellat in debitis, porro modi laudantium eos,
-      exercitationem laborum?
-    </p>
-  </div>
-</div>
-
-<div class="fog hidden"></div>
 
 <style>
+  .app {
+    position: fixed;
+    inset: 0;
+
+    background-color: black;
+  }
+
   .board {
     --grid-count: 31;
     --visible-tile-count: 5;
@@ -45,7 +56,7 @@
     --pan-y: 0;
     --zoom-scale: 0;
 
-    position: fixed;
+    position: absolute;
     inset: 0;
     overflow: hidden;
 
@@ -115,7 +126,7 @@
   .fog {
     --visible-radius: 500px;
 
-    position: fixed;
+    position: absolute;
     inset: 0;
 
     pointer-events: none;
@@ -137,7 +148,7 @@
   }
 
   .main_modal {
-    position: fixed;
+    position: absolute;
     top: 50%;
     left: 50%;
     translate: -50% -50%;
@@ -150,7 +161,9 @@
     align-items: center;
     gap: 1rem;
 
-    background-color: rgba(0 0 0 / 0.3);
+    color: white;
+
+    background-color: rgba(0 0 0 / 0.5);
     border-radius: 0.75vmin;
   }
 
